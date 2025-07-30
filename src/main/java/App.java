08eff0c0313beab1d397460ed622642c4c63bfa0
Drapsague <159650421+Drapsague@users.noreply.github.com>
2025-07-30 14:10:58 +0200

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,20 @@
-import java.io.*;
+import juliet.support.*;
+
+import javax.servlet.http.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.io.File;
+import java.io.IOException;
+
 import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import javax.xml.xpath.*;
+
 import org.xml.sax.InputSource;
+
+import org.apache.commons.lang.StringEscapeUtils;
 
 class IO {
     public static Logger logger = Logger.getLogger(IO.class.getName());
