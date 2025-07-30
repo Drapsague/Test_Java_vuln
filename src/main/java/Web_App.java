ea5735d7c0689b47008f662017813cf4332a7094
Drapsague <159650421+Drapsague@users.noreply.github.com>
2025-07-30 13:58:44 +0200

    Create Web_App.java

---
+++ src/main/java/Web_App.java
@@ -0,0 +1,226 @@
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.xpath.*;
+import org.xml.sax.InputSource;
+
+public class App {
+  private void ident() throws Throwable
+      {
+          String data;
+          if (privateReturnsTrue())
+          {
+              data = "";
+              {
+                  File file = new File("data.txt");
+                  FileInputStream streamFileInput = null;
+                  InputStreamReader readerInputStream = null;
+                  BufferedReader readerBuffered = null;
+                  try
+                  {
+                      streamFileInput = new FileInputStream(file);
+                      readerInputStream = new InputStreamReader(streamFileInput, "UTF-8");
+                      readerBuffered = new BufferedReader(readerInputStream);
+                    
+                      data = readerBuffered.readLine();
+                  }
+                  catch (IOException exceptIO)
+                  {
+                      IO.logger.log(Level.WARNING, "Error with stream reading", exceptIO);
+                  }
+                  finally
+                  {
+                      
+                      try
+                      {
+                          if (readerBuffered != null)
+                          {
+                              readerBuffered.close();
+                          }
+                      }
+                      catch (IOException exceptIO)
+                      {
+                          IO.logger.log(Level.WARNING, "Error closing BufferedReader", exceptIO);
+                      }
+  
+                      try
+                      {
+                          if (readerInputStream != null)
+                          {
+                              readerInputStream.close();
+                          }
+                      }
+                      catch (IOException exceptIO)
+                      {
+                          IO.logger.log(Level.WARNING, "Error closing InputStreamReader", exceptIO);
+                      }
+  
+                      try
+                      {
+                          if (streamFileInput != null)
+                          {
+                              streamFileInput.close();
+                          }
+                      }
+                      catch (IOException exceptIO)
+                      {
+                          IO.logger.log(Level.WARNING, "Error closing FileInputStream", exceptIO);
+                      }
+                  }
+              }
+          }
+          else
+          {
+             
+        
+              data = null;
+          }
+  
+          if (privateReturnsFalse())
+          {
+              
+              IO.writeLine("Benign, fixed string");
+          }
+          else
+          {
+  
+              String xmlFile = null;
+              if(System.getProperty("os.name").toLowerCase().indexOf("win") >= 0)
+              {
+          
+                  xmlFile = "\\src\\testcases\\ident2\\ident3.xml";
+              }
+              else
+              {
+                  
+                  xmlFile = "./src/testcases/ident2/ident3.xml";
+              }
+  
+              if (data != null)
+              {
+                 
+                  String [] tokens = data.split("||");
+                  if( tokens.length < 2 )
+                  {
+                      return;
+                  }
+                  String username = StringEscapeUtils.escapeXml(tokens[0]);
+                  String password = StringEscapeUtils.escapeXml(tokens[1]);
+        
+                  XPath xPath = XPathFactory.newInstance().newXPath();
+                  InputSource inputXml = new InputSource(xmlFile);
+                  String query = "//users/user[name/text()='" + username +
+                                 "' and pass/text()='" + password + "']" +
+                                 "/secret/text()";
+                  String secret = (String)xPath.evaluate(query, inputXml, XPathConstants.STRING);
+              }
+  
+          }
+      }
+  
+    
+      private void ident2() throws Throwable
+      {
+          String data;
+          if (privateReturnsTrue())
+          {
+              data = ""; 
+              {
+                  File file = new File("data.txt");
+                  FileInputStream streamFileInput = null;
+                  InputStreamReader readerInputStream = null;
+                  BufferedReader readerBuffered = null;
+                  try
+                  {
+                     
+                      streamFileInput = new FileInputStream(file);
+                      readerInputStream = new InputStreamReader(streamFileInput, "UTF-8");
+                      readerBuffered = new BufferedReader(readerInputStream);
+                    
+                      data = readerBuffered.readLine();
+                  }
+                  catch (IOException exceptIO)
+                  {
+                      IO.logger.log(Level.WARNING, "Error with stream reading", exceptIO);
+                  }
+                  finally
+                  {
+                      
+                      try
+                      {
+                          if (readerBuffered != null)
+                          {
+                              readerBuffered.close();
+                          }
+                      }
+                      catch (IOException exceptIO)
+                      {
+                          IO.logger.log(Level.WARNING, "Error closing BufferedReader", exceptIO);
+                      }
+  
+                      try
+                      {
+                          if (readerInputStream != null)
+                          {
+                              readerInputStream.close();
+                          }
+                      }
+                      catch (IOException exceptIO)
+                      {
+                          IO.logger.log(Level.WARNING, "Error closing InputStreamReader", exceptIO);
+                      }
+  
+                      try
+                      {
+                          if (streamFileInput != null)
+                          {
+                              streamFileInput.close();
+                          }
+                      }
+                      catch (IOException exceptIO)
+                      {
+                          IO.logger.log(Level.WARNING, "Error closing FileInputStream", exceptIO);
+                      }
+                  }
+              }
+          }
+          else
+          {
+             
+              data = null;
+          }
+  
+          if (privateReturnsTrue())
+          {
+              String xmlFile = null;
+              if(System.getProperty("os.name").toLowerCase().indexOf("win") >= 0)
+              {
+                
+                 xmlFile = "\\src\\testcases\\ident2\\ident3.xml";
+              }
+              else
+              {
+                  xmlFile = "./src/testcases/ident2/ident3.xml";
+              }
+              if (data != null)
+              {
+                  
+                  String [] tokens = data.split("||");
+                  if( tokens.length < 2 )
+                  {
+                      return;
+                  }
+                 
+                  String username = StringEscapeUtils.escapeXml(tokens[0]);
+                  String password = StringEscapeUtils.escapeXml(tokens[1]);
+                  
+                  XPath xPath = XPathFactory.newInstance().newXPath();
+                  InputSource inputXml = new InputSource(xmlFile);
+                  String query = "//users/user[name/text()='" + username +
+                                 "' and pass/text()='" + password + "']" +
+                                 "/secret/text()";
+                  String secret = (String)xPath.evaluate(query, inputXml, XPathConstants.STRING);
+              }
+          }
+      }
+}
